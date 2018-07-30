0cd6a9c6c93d1607dff6454d9d199aac98b04578
Danny Garcia <danielgarcia1812@gmail.com>
2018-07-30 17:41:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {// Keep track of index outside of the event handler.
+function init() {
+// Keep track of index outside of the event handler.
 let index = 0;
-  // Write your JavaScript code inside the init() function
 
 }
