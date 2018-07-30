ff5d917cf4b0d965cfe576050ff9a315b2eb1315
Danny Garcia <danielgarcia1812@gmail.com>
2018-07-30 17:45:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
-// Keep track of index outside of the event handler.
 let index = 0;
-// This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
+
+function init(e) {
   const key = parseInt(e.detail || e.which);
  
   if (key === code[index]) {
@@ -18,5 +15,4 @@ function onKeyDownHandler(e) {
   } else {
     index = 0;
   }
-}
 }
